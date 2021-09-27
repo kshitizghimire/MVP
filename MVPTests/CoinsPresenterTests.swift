ea27d067566e7c678470b1970af46d701da66d25
Tiz <kshitiz.ghimire@gmail.com>
2021-09-27 21:15:53 +1000

    Add tests

--- MVPTests/CoinsPresenterTests.swift
+++ MVPTests/CoinsPresenterTests.swift
@@ -25,7 +25,7 @@ final class CoinsPresenterTests: XCTestCase {
         presenter = nil
     }
 
-    func testViewDidLoad() {
+    func testViewDidLoad() throws {
         presenter.viewDidLoad()
 
         XCTAssertEqual(mockDisplay.title, "title")
@@ -33,9 +33,33 @@ final class CoinsPresenterTests: XCTestCase {
         XCTAssertNil(mockDisplay.sections)
 
         let coins = [
-            Coin(id: "1", symbol: "BTC", name: "Bitcoin", image: "https://test.com"),
+            Coin(id: "1", symbol: "BTC", name: "Bitcoin", image: "https://test.com/1"),
+            Coin(id: "2", symbol: "ETH", name: "Etherium", image: "https://test.com/2"),
+            Coin(id: "4", symbol: "ADA", name: "Cardano", image: "https://test.com/4"),
+            Coin(id: "3", symbol: "UNI", name: "UniSwap", image: "https://test.com/3"),
         ]
         mockModelLoader.completionHandler?(.success(coins))
         XCTAssertEqual(mockDisplay.sections?.count, 1)
+        let section: TableSection = try XCTUnwrap(mockDisplay.sections?[0])
+
+        let cells = section.cells
+        XCTAssertEqual(coins.count, 4)
+
+        struct Test {
+            let text: String
+            let imageUrl: String
+        }
+
+        let expectedCells = [
+            Test(text: "Bitcoin", imageUrl: "https://test.com/1"),
+            Test(text: "Etherium", imageUrl: "https://test.com/2"),
+            Test(text: "Cardano", imageUrl: "https://test.com/4"),
+            Test(text: "UniSwap", imageUrl: "https://test.com/3"),
+        ]
+        zip(cells, expectedCells).forEach {
+            let cell = $0 as? LabelAndImage
+            XCTAssertEqual(cell?.text, $1.text)
+            XCTAssertEqual(cell?.imageUrl, $1.imageUrl)
+        }
     }
 }
