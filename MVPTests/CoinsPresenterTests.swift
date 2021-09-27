1f9af6163d74610d4c49196451b63999588cee65
Tiz <kshitiz.ghimire@gmail.com>
2021-09-27 17:19:25 +1000

    Add section test

--- MVPTests/CoinsPresenterTests.swift
+++ MVPTests/CoinsPresenterTests.swift
@@ -3,19 +3,39 @@
 import XCTest
 
 final class CoinsPresenterTests: XCTestCase {
-    func testTitle() {
-        let modelLoader = MockModelLoader<[Coin]>()
-        let mockDisplay = MockTableDisplay()
-        let sut = CoinsPresenter(
+    var mockModelLoader: MockModelLoader<[Coin]>!
+    var mockDisplay: MockTableDisplay!
+    var presenter: CoinsPresenter!
+    override func setUpWithError() throws {
+        mockModelLoader = MockModelLoader<[Coin]>()
+        mockDisplay = MockTableDisplay()
+        presenter = CoinsPresenter(
             display: mockDisplay,
             title: "title",
-            modelLoader: modelLoader,
+            modelLoader: mockModelLoader,
             imageLoader: MockImageLoader(),
             apiUrl: URL(string: "http://test.com")!,
             cellPlaceholderImage: UIImage()
         )
-        sut.viewDidLoad()
+    }
+
+    override func tearDownWithError() throws {
+        mockModelLoader = nil
+        mockDisplay = nil
+        presenter = nil
+    }
+
+    func testViewDidLoad() {
+        presenter.viewDidLoad()
 
         XCTAssertEqual(mockDisplay.title, "title")
+
+        XCTAssertNil(mockDisplay.sections)
+
+        let coins = [
+            Coin(id: "1", symbol: "BTC", name: "Bitcoin", image: "https://test.com"),
+        ]
+        mockModelLoader.completionHandler?(.success(coins))
+        XCTAssertEqual(mockDisplay.sections?.count, 1)
     }
 }
